e67461ed463790308dc9a8fdaf3912291ef977f1
Łukasz Zięba <lukzieb@wp.pl>
2024-12-30 11:54:10 +0100

    add PublishingService.CredentialsEncryption

---
+++ SitecoreGroove.Plugin.PublishingService.CredentialsEncryption/ConfigureSitecore.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Sitecore.Framework.Runtime.Configuration;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SitecoreGroove.Plugin.PublishingService.CredentialsEncryption
+{
+    public sealed class ConfigureSitecore
+    {
+        public ConfigureSitecore(ISitecoreConfiguration scConfig)
+        {
+            IEnumerable<IConfigurationSection> connectionStringSections = scConfig.GetSection("Publishing:ConnectionStrings").GetChildren();
+
+            foreach (IConfigurationSection section in connectionStringSections.Where(x=>x.Key != "Service"))
+            {
+                scConfig[$"Publishing:ConnectionStrings:{section.Key}"] = Decrypt(section.Value);
+            }
+        }
+
+        private static string Decrypt(string value)
+        {
+            byte[] encryptedConnectionString = Convert.FromBase64String(value);
+            byte[] decryptedConnectionString = ProtectedData.Unprotect(encryptedConnectionString, null, DataProtectionScope.LocalMachine);
+
+            return Encoding.Unicode.GetString(decryptedConnectionString);
+        }
+    }
+}
